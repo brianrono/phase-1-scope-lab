5070764c557d8cbfa1d7a77e7ba889ac149e1cc2
brianrono <brianrono16@gmail.com>
2023-03-17 23:45:54 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+function setBestCustomer(){
+window.setBestCustomer;
+bestCustomer = "not bob";
+return bestCustomer;
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+
+
+const leastFavoriteCustomer = 'not you either!';
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'new';
+    return leastFavoriteCustomer;
+}
